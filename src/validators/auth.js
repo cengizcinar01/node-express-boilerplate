4dad0eeee8677fd40a412482e483354a572d10ca
Cengiz Cinar <c.cinar@mail.de>
2024-06-02 21:45:52 +0200

    refactor: improve code readability in auth.js
    Refactored auth.js for better readability. Replaced "check" with "body" from express-validator for better context understanding. Added more descriptive comments for each validation block. Corrected grammatical error in error message from "Email does not exists." to "Email does not exist.".

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -1,19 +1,19 @@
-const { check } = require("express-validator");
+const { body } = require("express-validator");
 const db = require("../db");
 const { compare } = require("bcryptjs");
 
-//password
-const password = check("password")
+// Password validation
+const password = body("password")
   .isLength({ min: 6, max: 15 })
   .withMessage("Password has to be between 6 and 15 characters.");
 
-//email
-const email = check("email")
+// Email validation
+const email = body("email")
   .isEmail()
   .withMessage("Please provide a valid email.");
 
-//check if email exists
-const emailExists = check("email").custom(async (value) => {
+// Check if email exists
+const emailExists = body("email").custom(async (value) => {
   const { rows } = await db.query("SELECT * from users WHERE email = $1", [
     value,
   ]);
@@ -23,20 +23,22 @@ const emailExists = check("email").custom(async (value) => {
   }
 });
 
-//login validation
-const loginFieldCheck = check("email").custom(async (value, { req }) => {
-  const user = await db.query("SELECT * from users WHERE email = $1", [value]);
-  if (!user.rows.length) {
-    throw new Error("Email does not exists.");
+// Login validation
+const loginFieldCheck = body("email").custom(async (value, { req }) => {
+  const { rows } = await db.query("SELECT * from users WHERE email = $1", [
+    value,
+  ]);
+  if (!rows.length) {
+    throw new Error("Email does not exist.");
   }
 
-  const validPassword = await compare(req.body.password, user.rows[0].password);
+  const validPassword = await compare(req.body.password, rows[0].password);
 
   if (!validPassword) {
     throw new Error("Wrong password.");
   }
 
-  req.user = user.rows[0];
+  req.user = rows[0];
 });
 
 module.exports = {
