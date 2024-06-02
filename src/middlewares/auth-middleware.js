0811007ba6d624f742d807a201d5c0bd772bc6b6
Cengiz Cinar <c.cinar@mail.de>
2024-06-02 21:59:05 +0200

    docs: Update comments in auth-middleware.js
    Updated the comments in the auth-middleware.js file to better explain the functionality of the userAuth export. It now includes information about the use of Passport.js and the JWT strategy, as well as the non-use of sessions.

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -1,3 +1,7 @@
 const passport = require("passport");
-
+/**
+ * Middleware to authenticate users using JWT.
+ * This middleware uses Passport.js with the JWT strategy.
+ * It ensures that the user is authenticated and the session is not used.
+ */
 exports.userAuth = passport.authenticate("jwt", { session: false });
